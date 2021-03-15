91747ef46b69247b5659093015c39d954f14e255
msaepululum <m.saepul.ulum@gmail.com>
2021-03-15 07:39:33 +0700

    create project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
